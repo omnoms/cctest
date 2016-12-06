0b98a923280491adb3d9471c452541a8858b5786
Marko Vainiokangas <marko.vainiokangas@atea.se>
2016-12-06 20:32:06 +0100

    bah

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 exports.creds = {
-  returnURL: 'https://localhost:3000/auth/openid/return', 
+  returnURL: 'https://ccmvwebapp.azurewebsites.net/auth/openid/return', 
   identityMetadata: 'https://login.microsoftonline.com/65f51067-7d65-4aa9-b996-4cc43a0d7111/.well-known/openid-configuration', 
   clientID: '7eaf21e4-242c-4422-9d4e-c464ce509711', 
   skipUserProfile: true, 
