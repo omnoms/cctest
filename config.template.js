f28cc7e925088676fc1af7cc5e9e65b54e1ad5d9
Marko Vainiokangas <marko.vainiokangas@atea.se>
2016-12-06 19:45:47 +0100

    mergefix

--- config.template.js
+++ config.template.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
  exports.creds = {
  	returnURL: 'https://XXXXX/auth/openid/return', //Configured in the Azure auth portal
  	identityMetadata: 'https://login.microsoftonline.com/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx/.well-known/openid-configuration', // Add the tender GUID
@@ -14,21 +13,4 @@
     server: 'xxxxxxxx',  
     // If you are on Microsoft Azure, you need this:  
     options: {encrypt: true, database: 'xxxxxx'}
-=======
- exports.creds = {
- 	returnURL: 'https://XXXXX/auth/openid/return', //Configured in the Azure auth portal
- 	identityMetadata: 'https://login.microsoftonline.com/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx/.well-known/openid-configuration', // Add the tender GUID
- 	clientID: 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx', //Application ID from Azure auth portal
- 	skipUserProfile: true, // for AzureAD should be set to true.
- 	responseType: 'id_token', // for login only flows use id_token. For accessing resources use `id_token code`
- 	responseMode: 'form_post' // For login only flows we should have token passed back to us in a POST
- };
-
-  exports.db = {
-    userName: 'xxxxx',  
-    password: 'xxxxx',  
-    server: 'xxxxxxxx',  
-    // If you are on Microsoft Azure, you need this:  
-    options: {encrypt: true, database: 'xxxxxx'}
->>>>>>> 9a216b3ac5e07bbfd147b1fe81da39cbc55f7e93
  };
--- public/js/vote.js
+++ public/js/vote.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /* vote.js
 Custom js to handle validation and submit of votes
 
@@ -15,22 +14,4 @@ $(function () {
             $('#errorMessage').text("Ange betyg och kommentar!");
         }
     });
-=======
-/* vote.js
-Custom js to handle validation and submit of votes
-
- */
-
-$(function () {
-    $("#saveButton").on('click', function () {
-
-        if(Number($('form').serializeArray()[0].value) != '' && $('form').serializeArray()[1].value != ''){
-            $('form').submit();
-            $('#errorMessage').text('');
-        }
-        else {
-            $('#errorMessage').text("Ange betyg och kommentar!");
-        }
-    });
->>>>>>> 9a216b3ac5e07bbfd147b1fe81da39cbc55f7e93
 });
